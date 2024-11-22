173e30af7ed33f94b98bc84d62ec85c0682b2ed9
malak-err <erraoudymalak1997@gmail.com>
2024-11-22 16:17:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,119 @@
 #include <stdio.h>
 #include <string.h>
-#define max_taches 100
+
+#define MAX_TACHES 100
+
+
 typedef struct {
-    char titre;
-    char description;
-    char date_echeance;
-    char priorité;
-}tache;
-TACHE tache[nb_taches];
-int nb_taches=0;
-void ajoutertache(){
-    if(nembretache < maxtache){
-        printf("entrer le titre de tache:");
-        scanf("%50S",tache[nb_tache].titre);
-        printf("donner une description : ");
-        scanf("%200S",tache[nb_tache].description);
-        printf("entrer une date echeance:");
-        scanf("%10d",tache[nb_tache].date_echeance);
-        printf("donner une priorité:");
-       scanf("%10S",tache[nb_tache].priorité);
-        nb_tache++.
-        printf("tache ajouté avec succés!\n");
-    }else{
-        printf("le nombre maximum des taches a été atteint");
+    char titre[100];
+    char description[200];
+    char dateEcheance[20];  
+    char priorite[10];    
+    int id ;
+} Tache;
+tache ap
+
+Tache taches[MAX_TACHES];
+int nombreDeTaches = 0;  
+
+void ajouterTache() {
+    if (nombreDeTaches < MAX_TACHES) {
+        printf("Entrez le titre de la tâche: ");
+        scanf("%100s", taches[nombreDeTaches].titre);
+
+        printf("Entrez la description de la tâche: ");
+        scanf("%200s", taches[nombreDeTaches].description);
+
+        printf("Entrez la date d'échéance (JJ/MM/AAAA): ");
+        scanf("%20s", taches[nombreDeTaches].dateEcheance); 
+
+        printf("Entrez la priorité (High/Low): ");
+        scanf("%10s", taches[nombreDeTaches].priorite); 
+
+        nombreDeTaches++;
+        printf("Tâche ajoutée avec succès!\n");
+    } else {
+        printf("Le nombre maximum de tâches a été atteint.\n");
     }
 }
-void main(){
-    printf("menu de gestion des taches\n");
-    printf("1:ajouter une tache\n");
-    printf("2:afficher toutes les taches\n");
-    printf("3:modifier une tache\n");
-    printf("4:suprimerune tache\n ");
-    printf("filtrer les taches par priorité\n");
-    printf("quitter");
+
+
+void afficherTaches() {
+    if (nombreDeTaches == 0 ) {
+        printf("Aucune tâche à afficher.\n");
+    } else {
+            printf("Liste des tâches:\n");
+        for (int i = 0; i < nombreDeTaches; i++) {
+            printf("\nTâche %d:\n", i + 1);
+            printf("Titre: %s\n", taches[i].titre);
+            printf("Description: %s\n", taches[i].description);
+            printf("Date d'échéance: %s\n", taches[i].dateEcheance);
+            printf("Priorité: %s\n", taches[i].priorite);
+        }
+       }
+    }
+}
+ 
+ void modifiertache (struct tache*t,int determin , int newvalue){;
+ switch(determin){
+     case 1:
+     strcpy(t->description,newvalue);
+     break;
+     case 2:
+     strcpy(t->dateEcheance,newvalue);
+     break;
+     case 3:
+     strcpy(t-> priorite,newvalue);
+     break;
+     
+     void suprimertache(struct tache*t){
+         t->id=0;
+         strcpy(t->titre,"");
+         strcpy(t->description,"");
+         strcpy(t->,dateEcheance,"");
+         strcpy(t->, priorite,"");
+         printf("supression avec succés.");
+     }
+     
+     void filtrertache (){
+         
+         
+     }
+     int main (){
+         int choix ;
+    printf("\nMenu de Gestion des Tâches:\n");
+    printf("1. Ajouter une tâche\n");
+    printf("2. Afficher toutes les tâches\n");
+    printf("3. Modifier une tâche\n");
+    printf("4. Supprimer une tâche\n");
+    printf("5. Filtrer les tâches par priorité\n");
+    printf("6. Quitter\n");
+    printf("Entrez votre choix: ");
+    scanf("%d",&choix);
+}do{
+    switch(choix);
+    case 1: ajouterTache();
+    break;
+    case 2: affichertaches();
+    break;
+    case 3:modifiertache();
+    break;
+    case 4:suprimmertache();
+    break;
+    case 5:filtertache();
+    break;
+}
+
+         
+     }
+     
+     
+     
+     
+     
+ }
+
+
     
 
 
